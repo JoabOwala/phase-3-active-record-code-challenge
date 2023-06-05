3d4860266ae9afe1c788ca46a577edd32a803147
JoabOwala <owalajoab@gmail.com>
2023-06-05 15:46:08 +0300

    Aggregate and Association Methods

--- app/models/review.rb
+++ app/models/review.rb
@@ -9,4 +9,40 @@ class Review < ActiveRecord::Base
     def product
         Product.find_by(id: product_id)
     end
+
+    def print_review
+        product_name = product.name
+        user_name = user.name
+        star_rating = star_rating
+        comment = comment
+    
+        puts "Review for #{product_name} by #{user_name}: #{star_rating}. #{comment}"
+    end
+
+    def leave_review(user, star_rating, comment)
+        reviews.create(user: user, star_rating: star_rating, comment: comment)
+    end
+
+    def print_all_reviews
+        puts "Reviews for #{name}:"
+    
+        reviews.each do |review|
+          user_name = review.user.name
+          star_rating = review.star_rating
+          comment = review.comment
+    
+          puts "Review by #{user_name}: #{star_rating}. #{comment}"
+        end
+    end
+
+    def average_rating
+        total_ratings = reviews.sum(:star_rating)
+        total_reviews = reviews.count
+    
+        return 0 if total_reviews.zero?
+    
+        average = total_ratings.to_f / total_reviews
+        average.round(2)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ActiveRecord::Base
         Product.joins(:reviews).where(reviews: { user_id: id })
     end
 
+    def favorite_product
+        reviews.order(star_rating: :desc).first&.product
+    end
+
+    def remove_reviews(product)
+        reviews.where(product: product).destroy_all
+    end
+
 end
